9b9abdedd49c78eaa96e6a7a8650a5ff5e0c0265
bogdanbso <bogdan@bitstudio.ro>
2026-07-21 23:11:50 +0300

    update general

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -1,3 +1,4 @@
-export const BRAND_NAME = 'Brand';
-export const CONTACT_EMAIL = 'contact@exemplu.ro';
-export const CONTACT_PHONE = '+40 700 000 000';
+export const BRAND_NAME = 'Tech cu Bogdan';
+export const SITE_URL = 'https://techcub.ro';
+export const CONTACT_EMAIL = 'contact@techcub.ro';
+export const CONTACT_PHONE = '+40 774 497 894';
