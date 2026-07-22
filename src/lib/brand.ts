3545f3a3665f30dc6bfa5c060cb0deb4a10d2848
bogdanbso <bogdan@bitstudio.ro>
2026-07-22 19:08:49 +0300

    update general

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -7,3 +7,26 @@ export const LEGAL_NAME = 'MURĂȘAN BOGDAN PFA';
 export const LEGAL_CUI = '54894279'; 
 export const LEGAL_REG_COM = 'F2026030758003';
 export const LEGAL_ADDRESS = 'Bulevardul Bucureștii Noi, Nr. 136, Parter, Ap. 5, București, Sectorul 1, România'; 
+
+export const SOCIAL_LINKS = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/techcub',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://linkedin.com/company/techcubogdan',
+  },
+  {
+    name: 'WhatsApp',
+    url: 'https://wa.me/40774497894',
+  },
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/techcub.ro/',
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/techcubogdan/',
+  },
+];
